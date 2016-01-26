bf9a0f85399040b2da898e9002e2cf94fd447a79
Alan Gardner <urfolomeus@gmail.com>
2016-01-26 00:29:46 +0000

    Adding tests, first pass

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 'use strict';
 
+var path = require('path');
+
 // Documentation for Brunch plugins:
 // https://github.com/brunch/brunch/blob/master/docs/plugins.md
 
 class ElmCompiler {
   constructor(config) {
-    this.config = config && config.plugins && config.plugins.elm;
+    this.config = config && config.plugins && config.plugins.elmBrunch;
+    this.defaultPublicFolder = config && config.paths && config.paths.public;
   }
 
   compile(file) {
-    console.log('Compiling: ', file);
-
     var childProcess = require('child_process')
-      , path         = require('path')
-      , elmFolder    = path.dirname(file.path)
-      , srcFile      = path.basename(file.path)
-      , outputFile   = 'web/static/vendor/' + srcFile.toLowerCase().replace('elm', 'js')
-      , command      = 'elm make --yes --output ' + outputFile + ' ' + srcFile;
+      , elmFolder    = this._getElmFolder(file.path)
+      , fileName     = path.basename(file.path)
+      , mainModules  = this.config.mainModules || [fileName]
+      , outputFolder = this._getOutputFolder();
 
-    childProcess.exec(command, {cwd: elmFolder}, function (error, stdout, stderr){
-      console.log('Result:', error, stdout, stderr);
+    mainModules.forEach(function (moduleName) {
+      var outputFile   = path.join(outputFolder, moduleName.toLowerCase().replace('elm', 'js'))
+        , command      = 'elm make --yes --output ' + outputFile + ' ' + moduleName;
+
+      childProcess.exec(command, {cwd: elmFolder}, function (error, stdout, stderr){
+        console.log('Result:', error, stdout, stderr);
+      });
     });
 
     return Promise.resolve(file);
+  }
+
+  _getElmFolder(filePath) {
+    var elmFolder          = this.config.elmFolder || ''
+      , normalisedFilePath = filePath.replace(new RegExp('^' + elmFolder + '[' + path.sep + ']?'), '');
+
+    return path.join(elmFolder, path.dirname(normalisedFilePath));
+  }
+
+  _getOutputFolder() {
+    var outputFolder = this.config.outputFolder;
+    return outputFolder || this.defaultPublicFolder || ''
   }
 }
 
